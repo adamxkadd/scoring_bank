4987f87f243337c698dcc0c82c5b21f69527c84f
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 22:31:13 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,23 +2,37 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
 @app.route('/about')
 def about():
     return 'About'
-
-# @app.route('/test', methods =['GET', 'POST'] )
-@app.route('/test')
-def test():
-	# my_data = {'text' : 'texte', 'int' : 123, 'timestamp' : time.time()}
-	# my_data_json = json.dumps(my_data)
-	# my_data = 1234
-	# return str(my_data)	
-
-
-	my_data = {'text' : 'texte', 'int' : 123, 'message' : 'Hello, Flask!'}
-	# Utilisez jsonify pour convertir le dictionnaire en JSON et renvoyez-le avec un code de statut 200 (OK)
+  
+@app.route('/', methods =['GET', 'POST'] )
+def home():
+	my_data = {'text' : 'texto', 'int' : 55555, 'message' : 'Helloxxx, Flask!'}
 	return jsonify(my_data)
+
+
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
+
+# @app.route('/about')
+# def about():
+#     return 'About'
+
+# # @app.route('/test', methods =['GET', 'POST'] )
+# @app.route('/test')
+# def test():
+# 	# my_data = {'text' : 'texte', 'int' : 123, 'timestamp' : time.time()}
+# 	# my_data_json = json.dumps(my_data)
+# 	# my_data = 1234
+# 	# return str(my_data)	
+
+
+# 	my_data = {'text' : 'texte', 'int' : 123, 'message' : 'Hello, Flask!'}
+# 	# Utilisez jsonify pour convertir le dictionnaire en JSON et renvoyez-le avec un code de statut 200 (OK)
+# 	return jsonify(my_data)
