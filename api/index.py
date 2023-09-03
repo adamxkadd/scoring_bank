e78b24b13256cc3d13f9ebf9fb662ed07e6d6ccd
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 21:45:37 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -15,5 +15,10 @@ def about():
 def test():
 	# my_data = {'text' : 'texte', 'int' : 123, 'timestamp' : time.time()}
 	# my_data_json = json.dumps(my_data)
-	my_data = 1234
-	return str(my_data)	
+	# my_data = 1234
+	# return str(my_data)	
+
+
+	my_data = {'text' : 'texte', 'int' : 123, 'message' : 'Hello, Flask!'}
+	# Utilisez jsonify pour convertir le dictionnaire en JSON et renvoyez-le avec un code de statut 200 (OK)
+	return jsonify(my_data)
