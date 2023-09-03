092142bf1b4c8b482bfcbcea718b770fd8216759
adamxkadd <143011477+adamxkadd@users.noreply.github.com>
2023-09-03 21:37:54 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ def about():
 # @app.route('/test', methods =['GET', 'POST'] )
 @app.route('/test')
 def test():
-	my_data = {'text' : 'texte', 'int' : 123, 'timestamp' : time.time()}
+	# my_data = {'text' : 'texte', 'int' : 123, 'timestamp' : time.time()}
 	# my_data_json = json.dumps(my_data)
-	
+	my_data = 1234
 	return my_data	
